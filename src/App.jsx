be2ec35dc8cff3086c89ca07914d4ded923e9755
livferz <liviafernandesfaria@gmail.com>
2024-08-09 09:04:47 -0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import Listagem from './pages/Listagem'
+
+
 const App = () => {
+
+const [color]= useState("#0000FF")
+document.body.style.backgroundColor = color
 return (
 <Routes>
-<Route path="/" />
+<Route path="/" element={<Listagem/>}/>
 </Routes>
 )
 }
